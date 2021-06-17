7c0ba1b966fd6c0cfada1d85596c51b668b2ecdf
Gene Kelley <eugenek@guildmortgage.net>
2021-06-17 11:37:53 -0700

    Add ThemeSetter and Counter components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import ThemeSetter from './ThemeSetter';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [darkTheme, setDarkTheme] = useState(() => localStorage.getItem('dark_theme') === 'true');
+
+    useEffect(() => {
+        localStorage.setItem('dark_theme', darkTheme);
+    }, [darkTheme]);
+
+    function handleToggleThemeClick() {
+        setDarkTheme(!darkTheme);
+        console.log('dark_theme set to: ' + darkTheme);
+    }
+
+    return <>
+        <div className={darkTheme ? 'dark' : 'light'}>
+            <h2>Welcome</h2>
+            <button onClick={handleToggleThemeClick}>Toggle Theme</button>
+            <Counter />
+        </div>
+    </>;
 }
 
 export default App;
